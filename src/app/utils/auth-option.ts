856a5d3d083d1014e34fe9c8732b5214f06821ea
josua-stng <josuasitanggang51@gmail.com>
2024-01-01 09:31:15 +0700

    feat: add auth options component

---
+++ src/app/utils/auth-option.ts
@@ -0,0 +1,22 @@
+import GoogleProvider from 'next-auth/providers/google';
+import GitHubProvider from 'next-auth/providers/github';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+import { Adapter } from 'next-auth/adapters';
+
+const prisma = new PrismaClient();
+const authOption = {
+  adapter: PrismaAdapter(prisma) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+};
+
+export { authOption };
